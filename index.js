5b85b5a27ea5ab6ba0003838b0323f69bb1593d4
JuandaMT <juandamt@outlook.es>
2023-07-07 09:59:02 +0200

    created get products

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const app = express()
 const PORT = 3000
-
+app.get("/products", (req,res)=>{
+    res.send('Get products')
+})
 app.listen(PORT, ()=> console.log(`Servidor levantado en el puerto ${PORT}`))
