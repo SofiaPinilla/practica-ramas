30c0f1f61b0c892939dd996fa186d7cc8d19722c
JuandaMT <juandamt@outlook.es>
2023-07-07 10:12:14 +0200

    endpoint get users created

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ const PORT = 3000
 app.get("/products", (req,res)=>{
     res.send('Get products')
 })
+app.get("/users", (req,res)=>{
+    res.send('Get users')
+})
 app.listen(PORT, ()=> console.log(`Servidor levantado en el puerto ${PORT}`))
