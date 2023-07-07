59734f70473a779871103db094aa1c6a1ac36466
SofiaPinilla <contact@sofiapinilla.com>
2023-07-07 10:02:59 +0200

    endpoint get categories implemented

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const express = require("express")
 const app = express()
 const PORT = 3000
 
+app.get("/categories",(req,res)=>{
+    res.send("GET CATEGORIES")
+})
+
 app.listen(PORT, ()=> console.log(`Servidor levantado en el puerto ${PORT}`))
